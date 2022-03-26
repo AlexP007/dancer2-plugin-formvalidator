1672d8e1a556994ed993111e2730442ff3bf04af
Alexander Panteleev <alexpan@cpan.org>
2022-03-26 15:58:29 +0300

    [i] Role validation

--- lib/Dancer2/Plugin/FormValidator.pm
+++ lib/Dancer2/Plugin/FormValidator.pm
@@ -1,9 +1,23 @@
-package FormValidator;
+package Dancer2::Plugin::FormValidator;
 
-use Dancer2::Plugin;
-use feature qw(signatures);
-no warnings qw(experimental::signatures);
+use Data::FormValidator;
+use Moo;
+use Data::Dumper;
+# use Dancer2::Plugin;
 
 our $VERSION = '0.1';
 
-true;
+has validator => (
+    is  => 'ro',
+    isa => sub {
+        my $object = shift;
+        my $role   = 'Dancer2::Plugin::FormValidator::Role::Validator';
+
+        if (not $object->does($role)) {
+            my $name = $object->meta->name;
+            Carp::croak "$name should implement $role\n";
+        }
+    },
+);
+
+1;
