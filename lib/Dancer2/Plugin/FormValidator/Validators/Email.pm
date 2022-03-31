e80724922751971c97fabec59700a39910fbb8b6
Alexander Panteleev <alexpan@cpan.org>
2022-03-31 19:40:18 +0300

    [i] Email validator
    Took 5 minutes

---
+++ lib/Dancer2/Plugin/FormValidator/Validators/Email.pm
@@ -0,0 +1,22 @@
+package Dancer2::Plugin::FormValidator::Validators::Email;
+
+use Moo;
+use Email::Valid;
+use namespace::clean;
+
+with 'Dancer2::Plugin::FormValidator::Role::Validator';
+
+sub message {
+    return {
+        en => '%s is not a valid email',
+    };
+}
+
+sub validate {
+    my ($field, $input) = @_;
+    my $email = $input->{$field};
+
+    return $email eq Email::Valid->address($email);
+}
+
+1;
